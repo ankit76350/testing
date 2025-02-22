32e7d13f1f4ae9aa89c6681267e454fc734260d0
ankit76350 <krankit01012003@gmail.com>
2025-02-22 10:26:32 +0530

    matchers

--- index.test.js
+++ index.test.js
@@ -12,3 +12,23 @@ describe("multible",()=>{
         expect(mltpl(2,5)).not.toBe(11);
     })
 })
+
+
+test("2 and 5 mutiply equl to 10", async()=>{
+        expect(mltpl(2,5)).toBe(10);
+})
+
+
+// this will pass
+test("Object eqaul", async()=>{
+    const data = {alpha : "A"};
+   data["beta"] = "B";
+    expect(data).toEqual({alpha:"A", beta:"B" });
+})
+
+// this will fail
+test("Object eqaul", async()=>{
+    const data = {alpha : "A"};
+   data["beta"] = "B";
+    expect(data).toBe({alpha:"A", beta:"B" }); // Deep equlity
+})
