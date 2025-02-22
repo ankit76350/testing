083f6c27477f3c906d6f6c74885bf6d638e73b96
ankit76350 <krankit01012003@gmail.com>
2025-02-22 10:31:16 +0530

    deep equlity and exacy equlity

--- index.test.js
+++ index.test.js
@@ -1,18 +1,5 @@
 const mltpl = require("./index")
 
-//Test suite
-describe("multible",()=>{
-    it("2 and 5 mutiply equl to 10", async()=>{
-        expect(mltpl(2,5)).toBe(10);
-    })
-    it("2 and 5 mutiply equl to 10", async()=>{
-        expect(mltpl(2,5)).not.toBe(5);
-    })
-    it("2 and 5 mutiply equl to 10", async()=>{
-        expect(mltpl(2,5)).not.toBe(11);
-    })
-})
-
 
 test("2 and 5 mutiply equl to 10", async()=>{
         expect(mltpl(2,5)).toBe(10);
