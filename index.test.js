4f249955f287b3c9d53a2aaeb854effae89428b4
ankit76350 <krankit01012003@gmail.com>
2025-02-22 10:35:58 +0530

    check null value

--- index.test.js
+++ index.test.js
@@ -1,21 +1,9 @@
-const mltpl = require("./index")
-
-
-test("2 and 5 mutiply equl to 10", async()=>{
-        expect(mltpl(2,5)).toBe(10);
-})
-
-
-// this will pass
-test("Object eqaul", async()=>{
-    const data = {alpha : "A"};
-   data["beta"] = "B";
-    expect(data).toEqual({alpha:"A", beta:"B" });
-})
-
 // this will fail
-test("Object eqaul", async()=>{
-    const data = {alpha : "A"};
-   data["beta"] = "B";
-    expect(data).toBe({alpha:"A", beta:"B" }); // Deep equlity
+test("NUll Value", async()=>{
+const value = null;
+expect(value).toBeNull()
+expect(value).toBeDefined()
+expect(value).not.toBeUndefined()
+expect(value).not.toBeTruthy()
+expect(value).toBeFalsy()
 })
