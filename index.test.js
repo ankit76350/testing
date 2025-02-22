0ab66cda6a4accf0d3908fefad5a8937c6da01df
ankit76350 <krankit01012003@gmail.com>
2025-02-22 10:37:48 +0530

    zero check

--- index.test.js
+++ index.test.js
@@ -1,7 +1,15 @@
-// this will fail
+
 test("NUll Value", async()=>{
 const value = null;
 expect(value).toBeNull()
+expect(value).toBeDefined()
+expect(value).not.toBeUndefined()
+expect(value).not.toBeTruthy()
+expect(value).toBeFalsy()
+})
+test("NUll Value", async()=>{
+const value = 0;
+expect(value).not.toBeNull()
 expect(value).toBeDefined()
 expect(value).not.toBeUndefined()
 expect(value).not.toBeTruthy()
