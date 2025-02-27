af8103385b8c5c48da813afdaabb62391d24b045
ankit76350 <krankit01012003@gmail.com>
2025-02-27 10:43:52 +0530

    now after creating mocking the test case are passed

--- index.test.js
+++ index.test.js
@@ -12,4 +12,16 @@
 // })
 
 
-// now i have created 
+// now i have created __mocking__ folder and inside __mocking__ i have crated mathUtils folder.
+
+jest.mock("./mathUtils")
+const { calculate } = require("./app");
+const { add } = require("./mathUtils");
+
+describe("calculate", () => {
+  test('calls add function with parameter ', () => {
+    calculate(1, 2, "add");
+    expect(add).toHaveBeenCalled();  // pass
+    expect(add).toHaveBeenCalledWith(1, 2); // pass
+  }) 
+})
